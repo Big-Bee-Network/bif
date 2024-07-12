74c7c6b3257a72e45e1db6d5b2ef030cf560fae3
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-07-12 17:50:56 -0500

    working towards detection scripts

---
+++ find-datasets-with-bees.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# selects records that refer to a scientificName that discoverlife recognizes
+#
+
+VERSION_ANCHOR=${1:-"hash://sha256/37bdd8ddb12df4ee02978ca59b695afd651f94398c0fe2e1f8b182849a876bb2"}
+REMOTES="https://linker.bio"
+
+preston cat --no-cache --remotes "${REMOTES}" ${VERSION_ANCHOR}\
+ | grep hasVersion\
+ | preston dwc-stream --no-cache --remote "${REMOTES}"\
+ | jq --raw-output '[.["http://rs.tdwg.org/dwc/terms/scientificName"],.["http://www.w3.org/ns/prov#wasDerivedFrom"]] | @tsv'\
+ | sed 's/^/\t/g'\
+ | nomer append discoverlife\
+ | grep -v NONE\
+ | cut -f3\
+ | grep -Eo "hash://[a-z0-9]+/[a-f0-9]+"\
+ | uniq
+
---
+++ find-datasets-with-interactions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# selects datasets that contains one of more interaction claims.
+#
+
+VERSION_ANCHOR=${1:-"hash://sha256/37bdd8ddb12df4ee02978ca59b695afd651f94398c0fe2e1f8b182849a876bb2"}
+REMOTES="https://linker.bio"
+
+preston cat --no-cache --remotes "${REMOTES}" ${VERSION_ANCHOR}\
+ | grep hasVersion\
+ | grep -Eo "hash://[a-z0-9]+/[a-f0-9]+"\
+ | sed -E "s/([a-f0-9]+)$/\1\t\1/g"\
+ | awk -F '\t' '{ print "{ \"citation\": \"" $1 "\", \"format\": \"dwca\", \"url\": \"https://linker.bio/" $1 "\" }" > "elton/" $2 "-globi.json" }'
