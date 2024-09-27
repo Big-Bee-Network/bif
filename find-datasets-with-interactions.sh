ba9adfb2e71264b183f7079f1f765c938cf7ff76
Katja Seltmann <enicospilus@gmail.com>
2024-09-27 09:38:47 -0700

    Update find-datasets-with-interactions.sh

--- find-datasets-with-interactions.sh
+++ find-datasets-with-interactions.sh
@@ -2,6 +2,8 @@
 #
 # selects datasets that contains one of more interaction claims.
 #
+# example of starting point that is used here
+# Poelen, J. H. (2023). A biodiversity dataset graph: GBIF, iDigBio, BioCASe hash://sha256/450deb8ed9092ac9b2f0f31d3dcf4e2b9be003c460df63dd6463d252bff37b55 hash://md5/898a9c02bedccaea5434ee4c6d64b7a2 (0.0.4) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.7651831
 
 VERSION_ANCHOR=${1:-"hash://sha256/37bdd8ddb12df4ee02978ca59b695afd651f94398c0fe2e1f8b182849a876bb2"}
 REMOTES="https://linker.bio"
