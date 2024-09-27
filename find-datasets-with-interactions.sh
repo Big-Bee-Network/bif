9c9c00b99fa3713c80a3518f1e67056d7d696aee
Katja Seltmann <enicospilus@gmail.com>
2024-09-27 09:48:59 -0700

    Update find-datasets-with-interactions.sh

--- find-datasets-with-interactions.sh
+++ find-datasets-with-interactions.sh
@@ -4,6 +4,7 @@
 #
 # example of starting point that is used here
 # Poelen, J. H. (2023). A biodiversity dataset graph: GBIF, iDigBio, BioCASe hash://sha256/450deb8ed9092ac9b2f0f31d3dcf4e2b9be003c460df63dd6463d252bff37b55 hash://md5/898a9c02bedccaea5434ee4c6d64b7a2 (0.0.4) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.7651831
+# Or list of other starting points are found here: https://linker.bio/#use-case-3-studying-pine-pests-caused-by-weevils-curculionoidea
 
 VERSION_ANCHOR=${1:-"hash://sha256/37bdd8ddb12df4ee02978ca59b695afd651f94398c0fe2e1f8b182849a876bb2"}
 REMOTES="https://linker.bio"
